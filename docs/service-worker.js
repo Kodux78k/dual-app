fee879e3094b130763fe305c9c1c3182ad77baf3
Kodux78k <koduxlive@gmail.com>
2025-06-15 04:05:50 -0300

    Atualizar o service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,15 +1,13 @@
-const CACHE_NAME = 'dualchat-v8-final';
+const CACHE_NAME = 'dualchat-v9-final';
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './splash.html',
-  './dual-chat-0-94-fix-64.html',
+  './dual-chat-0-94-fix-78-75-E-4.html',
   './dual-chat-0-94-fix-78-75.html',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png',
-  './data/Superfetch.json',
-  './data/site-structure.json'
 ];
 
 self.addEventListener('install', evt => {
