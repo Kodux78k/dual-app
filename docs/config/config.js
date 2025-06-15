f8c127d503d403cf5e6510f3ff6116788e03a51e
Kodux78k <koduxlive@gmail.com>
2025-06-15 00:59:53 -0300

    Add files via upload

---
+++ docs/config/config.js
@@ -0,0 +1,14 @@
+// ESTE ARQUIVO DEVE SER GERADO NO BUILD A PARTIR DOS SEUS .env.*
+// Coloque-o em /config/config.js e sirva-o estaticamente antes do index.html
+
+window.env = {
+  API_KEYS: [
+    // chave primária (.env.primary)
+    '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',
+    // chave de backup  (.env.backup)
+    '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',
+    // (adicione quantas chaves precisar)
+    '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',
+    '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',
+  ]
+};
---
+++ docs/config/configkey.js
@@ -0,0 +1,4 @@
+// config/configkey.js
+const CONFIG = {
+  AUTH_TOKEN: 'Bearer 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'
+};
